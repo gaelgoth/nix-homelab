1059a3b0b4cb520d58172b249745ff04797fc0f2
gaelgoth <gothuey.gael@gmail.com>
2024-09-22 18:09:24 +0200

    feat: try vscode server
    Signed-off-by: gaelgoth <gothuey.gael@gmail.com>

--- flake.nix
+++ flake.nix
@@ -2,11 +2,17 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.disko.url = "github:nix-community/disko";
   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.vscode-server.url = "github:nix-community/nixos-vscode-server";
 
-  outputs = { nixpkgs, disko, ... }: {
+  outputs = { nixpkgs, disko, vscode-server, ... }: {
     nixosConfigurations.nixos-anywhere-vm = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ disko.nixosModules.disko ./configuration.nix ];
+      modules = [
+        disko.nixosModules.disko
+        vscode-server.nixosModules.default
+        ./configuration.nix
+        ({ config, pkgs, ... }: { services.vscode-server.enable = true; })
+      ];
     };
   };
 }
