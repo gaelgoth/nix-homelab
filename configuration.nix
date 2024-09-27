06962d1fbcfca70f97806b2cfc8417c66db61042
gaelgoth <gothuey.gael@gmail.com>
2024-09-27 23:17:59 +0200

    feat: test nix-sops

--- configuration.nix
+++ configuration.nix
@@ -1,9 +1,35 @@
-{ modulesPath, config, lib, pkgs, ... }: {
+{ modulesPath, config, lib, pkgs, inputs, ... }: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
     ./disk-config.nix
   ];
+
+  sops = {
+    age.keyFile = "/root/.config/sops/age/keys.txt";
+    defaultSopsFile = ./secrets/secrets.yaml;
+    defaultSopsFormat = "yaml";
+  };
+
+  sops.secrets.example-key = { };
+  sops.secrets.example-key = { };
+  sops.secrets."myservice/my_subdir/my_secret" = { owner = "root"; };
+
+  systemd.services."root" = {
+    script = ''
+      echo "
+      Hey bro! I'm a service, and imma send this secure password:
+      $(cat ${config.sops.secrets."myservice/my_subdir/my_secret".path})
+      located in:
+      ${config.sops.secrets."myservice/my_subdir/my_secret".path}
+      to database and hack the mainframe
+      " > /var/lib/root/testfile
+    '';
+    serviceConfig = {
+      User = "root";
+      WorkingDirectory = "/var/lib/root";
+    };
+  };
 
   # allow unfree packages to be installed
   nixpkgs.config = { allowUnfree = true; };
@@ -55,6 +81,8 @@
     pkgs.curl
     pkgs.gitMinimal
     pkgs.nano
+    pkgs.sops
+    pkgs.ncdu
 
     # UNCOMMENT the following to install these packages systemwide
     # pkgs.jq
--- flake.nix
+++ flake.nix
@@ -1,15 +1,22 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.disko.url = "github:nix-community/disko";
-  inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.vscode-server.url = "github:nix-community/nixos-vscode-server";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
+    sops-nix.url = "github:Mic92/sops-nix";
+  };
 
-  outputs = { nixpkgs, disko, vscode-server, ... }: {
+  outputs = { self, nixpkgs, disko, vscode-server, sops-nix, ... }: {
     nixosConfigurations.nixos-anywhere-vm = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = { inherit sops-nix; };
       modules = [
         disko.nixosModules.disko
         vscode-server.nixosModules.default
+        sops-nix.nixosModules.sops
         ./configuration.nix
         ({ config, pkgs, ... }: { services.vscode-server.enable = true; })
       ];
