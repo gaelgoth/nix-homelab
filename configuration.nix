e27a8241ae056fafcc156fe83afa6a7662536d3b
Gaël G. <gothuey.gael@gmail.com>
2024-10-13 18:03:42 +0200

    feat: system auto upgrade

--- configuration.nix
+++ configuration.nix
@@ -114,5 +114,14 @@ in {
     };
   };
 
+
+  # inspo: https://github.com/reckenrode/nixos-configs/blob/main/hosts/meteion/configuration.nix
+  system.autoUpgrade = {
+    enable = true;
+    dates = "*-*-* 07:00:00";
+    randomizedDelaySec = "1h";
+    flake = "github:gaelgoth/nix-homelab";
+  };
+
   system.stateVersion = "23.11";
 }
