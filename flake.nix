454d32ca4e3c80b8bf5ab737fcbb0dc9105513fb
gaelgoth <gothuey.gael@gmail.com>
2024-10-18 12:00:21 +0400

    test comin

--- flake.nix
+++ flake.nix
@@ -5,44 +5,62 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    comin = {
+      url = "github:nlewo/comin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     vscode-server.url = "github:nix-community/nixos-vscode-server";
     sops-nix.url = "github:Mic92/sops-nix";
   };
 
-  outputs = { self, nixpkgs, disko, vscode-server, sops-nix, ... }: {
-    nixosConfigurations.nixos-homelab-vm = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = {
-        inherit sops-nix;
-        vars = import ./vars.nix;
+  outputs = { self, nixpkgs, disko, vscode-server, sops-nix, comin, ... }: {
+    nixosConfigurations = {
+      nixos-homelab-vm = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {
+          inherit sops-nix;
+          vars = import ./vars.nix;
+        };
+        modules = [
+          disko.nixosModules.disko
+          vscode-server.nixosModules.default
+          sops-nix.nixosModules.sops
+          comin.nixosModules.comin
+
+          ({ config, ... }: {
+            services.comin = {
+              enable = true;
+              remotes = [{
+                name = "origin";
+                url = "https://github.com/gaelgoth/nix-homelab.git";
+                branches.main.name = "main";
+                poller.period = 5; # seconds
+              }];
+            };
+          })
+
+          ./configuration.nix
+          ./modules/monitoring
+
+          ./containers/arr
+          ./containers/changedetection
+          ./containers/dockge
+          ./containers/dozzle
+          ./containers/grafana
+          ./containers/handbrake
+          ./containers/homepage
+          ./containers/neko
+          ./containers/pairdrop
+          ./containers/qbittorrent
+          ./containers/speedtest
+          ./containers/stirlingpdf
+          ./containers/uptimekuma
+
+          # ./containers/jellyfin
+
+          ({ config, pkgs, ... }: { services.vscode-server.enable = true; })
+        ];
       };
-      modules = [
-        disko.nixosModules.disko
-        vscode-server.nixosModules.default
-        sops-nix.nixosModules.sops
-
-        ./configuration.nix
-
-        ./modules/monitoring
-
-        ./containers/arr
-        ./containers/changedetection
-        ./containers/dockge
-        ./containers/dozzle
-        ./containers/grafana
-        ./containers/handbrake
-        ./containers/homepage
-        ./containers/neko
-        ./containers/pairdrop
-        ./containers/qbittorrent
-        ./containers/speedtest
-        ./containers/stirlingpdf
-        ./containers/uptimekuma
-
-        # ./containers/jellyfin
-
-        ({ config, pkgs, ... }: { services.vscode-server.enable = true; })
-      ];
     };
   };
 }
