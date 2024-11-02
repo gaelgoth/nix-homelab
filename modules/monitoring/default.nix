6ebe6e008f9dd1d55e239f9f5f0a8bf72f97b1d5
gaelgoth <gothuey.gael@gmail.com>
2024-11-02 20:49:53 +0100

    feat: nas metric

--- modules/monitoring/default.nix
+++ modules/monitoring/default.nix
@@ -10,19 +10,50 @@
   };
 
   services.prometheus = {
-    scrapeConfigs = [{
-      job_name = "node";
-      static_configs = [{
-        targets = [
-          "127.0.0.1:${toString config.services.prometheus.exporters.node.port}"
+    scrapeConfigs = [
+      {
+        job_name = "node";
+        static_configs = [{
+          targets = [
+            "127.0.0.1:${
+              toString config.services.prometheus.exporters.node.port
+            }"
+          ];
+        }];
+      }
+      {
+        job_name = "snmp";
+        scrape_interval = "30s";
+        static_configs = [{ targets = [ "192.168.1.2" ]; }];
+        metrics_path = "/snmp";
+        params = {
+          auth = [ "public_v3" ];
+          module = [ "synology" ];
+        };
+        relabel_configs = [
+          {
+            source_labels = [ "__address__" ];
+            target_label = "__param_target";
+          }
+          {
+            source_labels = [ "__param_target" ];
+            target_label = "instance";
+          }
+          {
+            target_label = "__address__";
+            replacement =
+              "192.168.1.2:9116"; # The SNMP exporter's real hostname:port
+          }
         ];
-      }];
-    }];
+      }
+
+    ];
     enable = true;
     port = 9001;
     exporters = {
       node = {
         enable = true;
+        enabledCollectors = [ "systemd" ];
         port = 9002;
       };
     };
