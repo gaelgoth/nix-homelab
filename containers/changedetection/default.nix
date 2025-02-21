78fbdbfb20b557679fcbadfc91f6bb9478e14f8f
gaelgoth <gothuey.gael@gmail.com>
2025-02-21 17:51:23 +0100

    chore: disable unused service

--- containers/changedetection/default.nix
+++ containers/changedetection/default.nix
@@ -17,28 +17,28 @@
       ];
       ports = [ "5000:5000" ];
       volumes = [ "changedetection-data:/datastore" ];
-      environment = { PLAYWRIGHT_DRIVER_URL = "ws://playwright-chrome:3000"; };
-      dependsOn = [ "playwright-chrome" ];
+      # environment = { PLAYWRIGHT_DRIVER_URL = "ws://playwright-chrome:3000"; };
+      # dependsOn = [ "playwright-chrome" ];
     };
 
-    playwright-chrome = {
-      image = "dgtlmoon/sockpuppetbrowser:latest";
-      autoStart = true;
-      extraOptions = [ "--pull=newer" ];
-      environment = {
-        SCREEN_WIDTH = "1920";
-        SCREEN_HEIGHT = "1024";
-        SCREEN_DEPTH = "16";
-        MAX_CONCURRENT_CHROME_PROCESSES = "10";
-        ENABLE_DEBUGGER = "false";
-        PREBOOT_CHROME = "true";
-        CONNECTION_TIMEOUT = "300000";
-        MAX_CONCURRENT_SESSIONS = "10";
-        CHROME_REFRESH_TIME = "600000";
-        DEFAULT_BLOCK_ADS = "true";
-        DEFAULT_STEALTH = "true";
-        DEFAULT_IGNORE_HTTPS_ERRORS = "true";
-      };
-    };
+    # playwright-chrome = {
+    #   image = "dgtlmoon/sockpuppetbrowser:latest";
+    #   autoStart = true;
+    #   extraOptions = [ "--pull=newer" ];
+    #   environment = {
+    #     SCREEN_WIDTH = "1920";
+    #     SCREEN_HEIGHT = "1024";
+    #     SCREEN_DEPTH = "16";
+    #     MAX_CONCURRENT_CHROME_PROCESSES = "10";
+    #     ENABLE_DEBUGGER = "false";
+    #     PREBOOT_CHROME = "true";
+    #     CONNECTION_TIMEOUT = "300000";
+    #     MAX_CONCURRENT_SESSIONS = "10";
+    #     CHROME_REFRESH_TIME = "600000";
+    #     DEFAULT_BLOCK_ADS = "true";
+    #     DEFAULT_STEALTH = "true";
+    #     DEFAULT_IGNORE_HTTPS_ERRORS = "true";
+    #   };
+    # };
   };
 }
