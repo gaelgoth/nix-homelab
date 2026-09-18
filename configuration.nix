a23b8a2d588c0f887f07f2f8fa12e513eea00c29
gaelgoth <gothuey.gael@gmail.com>
2026-09-18 09:42:10 +0200

    feat: Use caches

--- configuration.nix
+++ configuration.nix
@@ -39,10 +39,20 @@ in
     efiInstallAsRemovable = true;
   };
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDvhrs="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 
   networking = {
     hostName = "nixos-homelab-vm";
